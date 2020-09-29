3ec41eb60dda212a82580daf982264e1a0a69942
CodingMirec <m.sykora01@gmail.com>
2020-09-29 21:36:38 +0200

    Sign up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Register from "./Register";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Register.js
@@ -0,0 +1,94 @@
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+import { Link } from "react-router-dom";
+
+import "./Register.css";
+
+function Register() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-10 col-xl-9 mx-auto my-auto">
+          <div className="card card-signin flex-row my-5">
+            <div className="card-img-left d-none d-md-flex">
+              {/* <!-- Background image for card set in CSS! --> */}
+            </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">Register</h5>
+              <form className="form-signin">
+                <div className="form-label-group">
+                  <input
+                    type="text"
+                    id="inputUserame"
+                    className="form-control"
+                    placeholder="Username"
+                    required
+                    autoFocus
+                  />
+                  <label htmlFor="inputUserame">Username</label>
+                </div>
+                <div className="form-label-group">
+                  <input
+                    type="email"
+                    id="inputEmail"
+                    className="form-control"
+                    placeholder="Email address"
+                    required
+                  />
+                  <label htmlFor="inputEmail">Email address</label>
+                </div>
+                <hr />
+                <div className="form-label-group">
+                  <input
+                    type="password"
+                    id="inputPassword"
+                    className="form-control"
+                    placeholder="Password"
+                    required
+                  />
+                  <label htmlFor="inputPassword">Password</label>
+                </div>
+                <div className="form-label-group">
+                  <input
+                    type="password"
+                    id="inputConfirmPassword"
+                    className="form-control"
+                    placeholder="Password"
+                    required
+                  />
+                  <label htmlFor="inputConfirmPassword">Confirm password</label>
+                </div>
+                <Link to="/">
+                  <button
+                    className="btn btn-lg btn-primary btn-block text-uppercase"
+                    type="submit"
+                  >
+                    Register
+                  </button>
+                </Link>
+                <Link to="/login">
+                  <p className="d-block text-center mt-2 small">Log In</p>
+                </Link>
+                <hr className="my-4" />
+                <button
+                  className="btn btn-lg btn-google btn-block text-uppercase"
+                  type="submit"
+                >
+                  <i className="fab fa-google"></i> Sign up with Google
+                </button>
+                <button
+                  className="btn btn-lg btn-facebook btn-block text-uppercase"
+                  type="submit"
+                >
+                  <i className="fab fa-facebook r-2"></i> Sign up with Facebook
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
